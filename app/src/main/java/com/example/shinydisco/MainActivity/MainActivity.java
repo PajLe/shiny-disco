57e3c078fb9f54be3de645e6a1e489a519430222
Pavle Aleksov <pavle.a@codemancystudio.com>
2021-06-02 20:25:09 +0200

    move main activity to separate package

--- app/src/main/java/com/example/shinydisco/MainActivity/MainActivity.java
+++ app/src/main/java/com/example/shinydisco/MainActivity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.shinydisco;
+package com.example.shinydisco.MainActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.shinydisco.R;
 
 public class MainActivity extends AppCompatActivity {
 
