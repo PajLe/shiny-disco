3c9708954565f52a7e6770dba2a182cf3b214984
Pavle Aleksov <pavle.a@codemancystudio.com>
2021-06-06 15:27:10 +0200

    finish SignInFragment

--- app/src/main/java/ui/MainActivity/SignInFragment.java
+++ app/src/main/java/ui/MainActivity/SignInFragment.java
@@ -1,33 +1,44 @@
 package ui.MainActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.viewpager2.widget.ViewPager2;
 
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.shinydisco.R;
-import com.google.android.material.tabs.TabLayout;
-import com.google.android.material.tabs.TabLayoutMediator;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Date;
+
+import data.User;
+import ui.HomeActivity.HomeActivity;
+import utils.Firebase;
+
+import static android.content.ContentValues.TAG;
+
 public class SignInFragment extends Fragment {
 
     private FirebaseAuth firebaseAuth;
 
-    private EditText usernameEditText;
+    private EditText emailEditText;
     private EditText passwordEditText;
     private Button signInButton;
 
@@ -49,6 +60,7 @@ public class SignInFragment extends Fragment {
         FirebaseUser currentUser = firebaseAuth.getCurrentUser();
         if(currentUser != null){
             currentUser.reload();
+            updateUI(currentUser);
         }
     }
 
@@ -62,12 +74,15 @@ public class SignInFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        usernameEditText = getView().findViewById(R.id.login_username_input);
+        emailEditText = getView().findViewById(R.id.login_email_input);
         passwordEditText = getView().findViewById(R.id.login_password_input);
         signInButton = getView().findViewById(R.id.login_button);
 
-        usernameEditText.addTextChangedListener(signUpTextWatcher);
+        // listeners
+        emailEditText.addTextChangedListener(signUpTextWatcher);
         passwordEditText.addTextChangedListener(signUpTextWatcher);
+
+        signInButton.setOnClickListener(signInButtonOnClickListener);
     }
 
     private TextWatcher signUpTextWatcher = new TextWatcher() {
@@ -78,10 +93,10 @@ public class SignInFragment extends Fragment {
 
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
-            String usernameInput = usernameEditText.getText().toString().trim();
+            String emailInput = emailEditText.getText().toString().trim();
             String passwordInput = passwordEditText.getText().toString().trim();
 
-            signInButton.setEnabled(!usernameInput.isEmpty()
+            signInButton.setEnabled(!emailInput.isEmpty()
                     && !passwordInput.isEmpty());
         }
 
@@ -90,4 +105,37 @@ public class SignInFragment extends Fragment {
 
         }
     };
+
+    private View.OnClickListener signInButtonOnClickListener = v -> {
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            String uid = user.getUid();
+
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(getActivity(), "Authentication failed: " + task.getException().getMessage(),
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    };
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            Intent intent = new Intent(getActivity(), HomeActivity.class);
+            getActivity().finish();
+            startActivity(intent);
+        }
+    }
 }
