9b8a505145e9e946300c101c104ae3ae3163eb69
Pavle Aleksov <pavle.a@codemancystudio.com>
2021-06-10 23:56:08 +0200

    remove notification bar on MainActivity

--- app/src/main/java/ui/MainActivity/MainActivity.java
+++ app/src/main/java/ui/MainActivity/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import com.example.shinydisco.R;
 import com.google.android.material.tabs.TabLayout;
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         // setup auth (sign in and sign up) tabs
