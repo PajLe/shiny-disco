ed71dc007adca728961ff81dde3690de22870a4e
Pavle Aleksov <pavle.a@codemancystudio.com>
2021-06-22 16:03:46 +0200

    add service starting in home activity

--- app/src/main/java/ui/HomeActivity/HomeActivity.java
+++ app/src/main/java/ui/HomeActivity/HomeActivity.java
@@ -7,9 +7,13 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+import androidx.preference.SwitchPreferenceCompat;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -37,6 +41,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import data.User;
+import services.BackgroundLocationService;
 import ui.MainActivity.MainActivity;
 import ui.SettingsScreen.SettingsFragment;
 import ui.ViewProfileScreen.ViewProfileFragment;
@@ -99,6 +104,14 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
                 .setReorderingAllowed(true)
                 .addToBackStack(null)
                 .commit();
+
+        SharedPreferences sharedPreferences =
+                PreferenceManager.getDefaultSharedPreferences(this);
+        boolean locServiceStart = sharedPreferences.getBoolean("notifications", false);
+        if (locServiceStart) {
+            Intent serv = new Intent(this, BackgroundLocationService.class);
+            startService(serv);
+        }
     }
 
     private void initializeUserInNavBar() {
