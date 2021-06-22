77a5f36f1266edd4da6a144479343525741cbe47
Pavle Aleksov <pavle.a@codemancystudio.com>
2021-06-22 16:21:15 +0200

    add notifications when nearby a disco

--- app/src/main/java/services/BackgroundLocationService.java
+++ app/src/main/java/services/BackgroundLocationService.java
@@ -1,22 +1,34 @@
 package services;
 
 import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Build;
 import android.os.IBinder;
 import android.os.Looper;
 
 import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import com.example.shinydisco.R;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
 
+import data.Disco;
+import ui.MainActivity.MainActivity;
 import utils.Firebase;
 
 public class BackgroundLocationService extends Service {
@@ -24,6 +36,8 @@ public class BackgroundLocationService extends Service {
     private LocationRequest locationRequest;
     private LocationCallback locationCallback;
     private FusedLocationProviderClient fusedLocationClient;
+    private final String CHANNEL_ID = "disco_notification_channel";
+    private NotificationChannel channel;
 
     public BackgroundLocationService() {
     }
@@ -33,6 +47,7 @@ public class BackgroundLocationService extends Service {
         super.onCreate();
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         createLocationRequest();
+        createNotificationChannel();
         locationCallback = new LocationCallback() {
             @Override
             public void onLocationResult(LocationResult locationResult) {
@@ -45,10 +60,62 @@ public class BackgroundLocationService extends Service {
                     String currentUserId = loggedInUser.getUid();
                     Firebase.getDbRef().child(Firebase.DB_USERS).child(currentUserId).child("lat").setValue(lastLocation.getLatitude());
                     Firebase.getDbRef().child(Firebase.DB_USERS).child(currentUserId).child("lon").setValue(lastLocation.getLongitude());
+                    getNearbyDiscos(lastLocation);
                 }
             }
         };
     }
+
+    private void getNearbyDiscos(Location lastLocation) {
+        Firebase.getDbRef().child(Firebase.DB_DISCOS).get().addOnSuccessListener(snapshot -> {
+           for (DataSnapshot child : snapshot.getChildren()) {
+               Disco disco = child.getValue(Disco.class);
+               Location loc = new Location("");
+
+               loc.setLatitude(disco.getLat());
+               loc.setLongitude(disco.getLon());
+               if (lastLocation.distanceTo(loc) < 1000.0f) {
+                   createNotification();
+                   break;
+               }
+           }
+        });
+    }
+
+    private void createNotification() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(BackgroundLocationService.this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.outline_nightlife_20)
+                .setContentTitle("You just passed a disco!")
+                .setContentText("There's a disco nearby. Tap for more info.")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        int notificationId = 3;
+        notificationManager.notify(notificationId, builder.build());
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.disco_notification_channel_name);
+            String description = getString(R.string.disco_notification_channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
