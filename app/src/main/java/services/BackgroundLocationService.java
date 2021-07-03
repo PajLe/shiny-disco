3e0b29bc90ac63d352e640c83809aa263ef1d92e
Pavle Aleksov <pavle.a@codemancystudio.com>
2021-07-03 20:39:23 +0200

    try

--- app/src/main/java/services/BackgroundLocationService.java
+++ app/src/main/java/services/BackgroundLocationService.java
@@ -119,7 +119,7 @@ public class BackgroundLocationService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         startLocationUpdates();
-        return super.onStartCommand(intent, flags, startId);
+        return START_STICKY;
     }
 
     @Override
